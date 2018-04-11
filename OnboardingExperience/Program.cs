0ff2a2a33f5864f1ac81c560fed4734b2be68c5e
Cody Winton <cody.t.winton@gmail.com>
2018-04-11 09:14:36 -0500

    Changed output

--- OnboardingExperience/Program.cs
+++ OnboardingExperience/Program.cs
@@ -6,7 +6,7 @@ namespace OnboardingExperience
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Goodbye World!");
 
             Console.ReadLine();
         }
